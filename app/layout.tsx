bc90feb0ecf35a8402b82586e2225bc5cbfae1cc
petervannord <132039111+petervannord@users.noreply.github.com>
2025-09-11 15:11:00 -0400

    new webhook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 export const metadata: Metadata = {
   title: "G4be Portfolio | Home",
   description: "Discover my portfolio, featuring my projects and skills.",
